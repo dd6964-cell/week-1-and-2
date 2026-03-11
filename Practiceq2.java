fc7c828fdb824814b24c9a1a755c917f783038c0
dd6964-cell <dd6964@srmist.edu.in>
2026-03-11 13:44:41 +0530

    Week1

---
+++ Practiceq2.java
@@ -0,0 +1,9 @@
+
+public class Practiceq2 {
+    public static void main(String[]args){
+        float dist=10.8f;
+        float mile =(dist*1.6f);
+        System.out.format("The distance 10.8 km in miles is %.2f",mile);
+    }
+    
+}
